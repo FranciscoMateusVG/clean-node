4cbc0d012605d72f5f2d45e37be839fc736d307b
FranciscoMateusVG <49985463+FranciscoMateusVG@users.noreply.github.com>
2022-06-16 21:52:17 -0300

    refactor:  db add account

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,17 +1,32 @@
+import { Encrypter } from '../protocols/encrypter'
 import { DbAddAccount } from './db-add-account'
+
+type ReturnMakeDbAddAccount = {
+  dbAddAccount: DbAddAccount
+  encrypterStub: Encrypter
+}
+
+const makeEncrypter = (): Encrypter => {
+  class EncrypterStub implements Encrypter {
+    async encrypt(value: string): Promise<string> {
+      return new Promise((resolve, reject) => {
+        resolve('hashed_password')
+      })
+    }
+  }
+  return new EncrypterStub()
+}
+
+const makeDbAddAccount = (): ReturnMakeDbAddAccount => {
+  const encrypterStub = makeEncrypter()
+  const dbAddAccount = new DbAddAccount(encrypterStub)
+
+  return { dbAddAccount, encrypterStub }
+}
 
 describe('DbAddAccount Usecase', () => {
   test('Should call Encrypter with correct password', async () => {
-    class EncrypterStub {
-      async encrypt(value: string): Promise<string> {
-        return new Promise((resolve, reject) => {
-          resolve('hashed_password')
-        })
-      }
-    }
-
-    const encrypterStub = new EncrypterStub()
-    const dbAddAccount = new DbAddAccount(encrypterStub)
+    const { dbAddAccount, encrypterStub } = makeDbAddAccount()
     const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
     const accountData = {
       name: 'name',
