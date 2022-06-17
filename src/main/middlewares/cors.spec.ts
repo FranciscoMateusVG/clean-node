798f6d49bfe5ba7d13347159e20778529535e9cf
FranciscoMateusVG <49985463+FranciscoMateusVG@users.noreply.github.com>
2022-06-17 15:13:46 -0300

    feat: made content type

--- src/main/middlewares/cors.spec.ts
+++ src/main/middlewares/cors.spec.ts
@@ -3,7 +3,7 @@ import app from '../config/app'
 
 describe('Cors Middleware', () => {
   test('is allowing cors', async () => {
-    app.post('/test_cors', (req, res) => {
+    app.get('/test_cors', (req, res) => {
       res.send()
     })
     await request(app)
