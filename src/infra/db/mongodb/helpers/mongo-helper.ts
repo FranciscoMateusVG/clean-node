3a3e0753f88984f0414026609e773e6169bdbb4f
FranciscoMateusVG <49985463+FranciscoMateusVG@users.noreply.github.com>
2022-07-05 22:55:58 -0300

    test: test if connection is null

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -3,16 +3,21 @@ import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient,
+  url: null as string,
 
   async connect(url: string): Promise<void> {
+    this.url = url
     const mongoClient = new MongoClient(url)
     this.client = await mongoClient.connect()
   },
 
   async disconnect(): Promise<void> {
     await this.client.close()
+    this.client = null
   },
-  getCollection(name: string): Collection {
+
+  async getCollection(name: string): Promise<Collection> {
+    if (!this.client) await this.connect(this.url)
     return this.client.db().collection(name)
   }
 }
